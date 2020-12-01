b43710b0b153d4742dc24dd1cb3508f74ee73549
eduardo leite <eduardo.leite@Mac-mini-de-eduardo.local>
2020-12-01 20:01:39 -0300

    REO4

--- fortnight03/cpu/basiccpu/BasicCPU.cpp
+++ fortnight03/cpu/basiccpu/BasicCPU.cpp
@@ -123,10 +123,10 @@ int BasicCPU::ID()
 			return decodeDataProcReg();
 			break;
 		
-		// TODO
-		// implementar o GRUPO A SEGUIR
-		//
-		// x111 Data Processing -- Scalar Floating-Point and Advanced SIMD on page C4-288
+		case 0x0E000000:
+		case 0x1E000000:
+			return decodeDataProcFloat();
+			break;
 
 		
 		// ATIVIDADE FUTURA
@@ -347,6 +347,38 @@ int BasicCPU::decodeDataProcFloat() {
 			
 			return 0;
 
+		case 0x1E202800:
+		//case 0x1E202820:
+
+			if (IR & 0x00C00000) return 1;
+
+			fpOp = FPOpFlag::FP_REG_32;
+
+			// ler A e B
+			n = (IR & 0x000003E0) >> 5;
+			A = getSasInt(n); // 32-bit variant
+
+			m = (IR & 0x001F0000) >> 16;
+			B = getSasInt(m);
+
+			// registrador destino
+			d = (IR & 0x0000001F);
+			Rd = &(V[d]);
+
+			// atribuir ALUctrl
+			ALUctrl = ALUctrlFlag::ADD;
+
+			// atribuir MEMctrl
+			MEMctrl = MEMctrlFlag::MEM_NONE;
+
+			// atribuir WBctrl
+			WBctrl = WBctrlFlag::RegWrite;
+
+			// atribuir MemtoReg
+			MemtoReg = false;
+
+			return 0;
+
 		default:
 			// instrução não implementada
 			return 1;
@@ -381,6 +413,9 @@ int BasicCPU::EXI()
 	{
 		case ALUctrlFlag::SUB:
 			ALUout = A - B;
+			return 0;
+		case ALUctrlFlag::ADD:
+			ALUout = A + B;
 			return 0;
 		default:
 			// Controle não implementado
@@ -421,6 +456,9 @@ int BasicCPU::EXF()
 		{
 			case ALUctrlFlag::SUB:
 				ALUout = Util::floatAsUint64Low(fA - fB);
+				return 0;
+			case ALUctrlFlag::ADD:
+				ALUout = Util::floatAsUint64Low(fA + fB);
 				return 0;
 			default:
 				// Controle não implementado
